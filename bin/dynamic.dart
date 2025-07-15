3b6e2034942e6ffd3556e292dc769125d43ba1cd
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2025-07-15 16:04:36 -0500

    done

---
+++ bin/dynamic.dart
@@ -0,0 +1,10 @@
+// dynamic bypass the type checking
+
+
+void main(){
+  dynamic myVariable = 10;
+
+  myVariable = 'hello';
+
+  print(myVariable);
+}
---
+++ bin/map.dart
@@ -0,0 +1,37 @@
+// map is called the object in dart
+// this is how map is working
+
+
+void main(){
+  Map ages = {
+    'name' :'sakib',
+    'age' : 23
+  };
+
+  print(ages);
+
+
+  Map friends = {
+    'name' : 'sakib',
+    'friend2' : 'omar',
+    
+  };
+  
+  print(friends);
+
+
+  Map<String , int> age = {
+    'Alice' : 45,
+    'Bob' : 90,
+    'charlie' : 35
+
+
+  };
+  print(age);
+
+}
+
+
+
+
+
