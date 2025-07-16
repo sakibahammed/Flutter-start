9a3cca8f3fe66b904b899f0e4160ad0e9477ff42
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2025-07-16 09:58:51 -0500

    if-else

---
+++ bin/controllFlow_ifElse.dart
@@ -0,0 +1,21 @@
+// marks challenge taking
+
+/* as the marks challenge is like :
+you got 80 = A , 70 = B , 60 = c , 50 = D
+ */
+
+
+void main(){
+  var number = 100;
+
+  if(number>=90 && number <=100){
+    print('you get A');
+  }
+  else if(number>=80 && number<=89){
+    print('you get B');
+  }
+
+  else{
+    print('basically fail');
+  }
+}
---
+++ bin/practices.dart
@@ -0,0 +1,9 @@
+void main(){
+  int age = 30;
+  double height = 5.9;
+  String name = 'sakib';
+  bool isStudent = false;
+
+
+  print(isStudent);
+}
