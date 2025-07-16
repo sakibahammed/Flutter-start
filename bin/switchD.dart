923191ccf84a94cc38eb608a669c7201c8fc5137
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2025-07-16 10:21:10 -0500

    done

---
+++ bin/switchD.dart
@@ -0,0 +1,22 @@
+void main(){
+  String grade = 'B-';
+
+  switch(grade){
+    case 'A':
+      print('Good');
+      break;
+
+    case 'B':
+      print('OK');
+      break;
+
+    case 'C':
+      print('Done');
+      break;
+
+    default :
+      print('Vaag ekhan theke');
+
+
+  }
+}
---
+++ bin/while_loop.dart
@@ -0,0 +1,8 @@
+void main(){
+  int count = 0;
+  while( count <9){
+    print('count : $count');
+    count++;
+  }
+}
+
